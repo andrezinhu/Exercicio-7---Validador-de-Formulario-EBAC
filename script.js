41e4d3f3a35b623b0fb8970423f3400bb10bbe8f
André Filipe <07andrefilipe@gmail.com>
2025-01-30 22:37:51 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('formValidacao');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const numero1 = document.getElementById('Campo A');
+    const numero2 = document.getElementById('Campo B');
+
+    const mensagemSucesso = "Parabéns, seu formulario foi validado com sucesso";
+    const mensagemFalha = "Infelizmente seu formulario não pode ser validado";
+
+    const containermensagemSucesso = document.querySelector('#mensagemSucesso');
+    const containermensagemFalha = document.querySelector('#mensagemFalha');
+
+    if(numero1.value > numero2.value){
+        containermensagemSucesso.style.display = 'none'
+        const containermensagemFalha = document.querySelector('#mensagemFalha');
+        containermensagemFalha.innerHTML = mensagemFalha;
+        containermensagemFalha.style.display = 'block';
+    } else {
+        containermensagemFalha.style.display = 'none';
+        const containermensagemSucesso = document.querySelector('#mensagemSucesso');
+        containermensagemSucesso.innerHTML = mensagemSucesso;
+        containermensagemSucesso.style.display = 'block'
+    }
+})
